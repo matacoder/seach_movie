4d25db2b3861b0a7f70f108130a25874dc3db2d3
Denis Matakov <matakov@gmail.com>
2021-05-11 18:00:02 +0300

    Separate RestClient and search function.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import os
+from dataclasses import dataclass
 
 import fire
 import requests
@@ -10,6 +11,30 @@ load_dotenv()
 
 TOKEN = os.getenv('TOKEN')
 SERVER = os.getenv('SERVER')
+ENDPOINT = '/lookup'
+
+
+@dataclass
+class RestClient:
+    server: str
+    endpoint: str
+    token: str
+
+    def search_term(self, term):
+        headers = {
+            'X-AppKey': self.token,
+        }
+        params = {
+            'term': term,
+        }
+        try:
+            r = requests.get(self.server + self.endpoint, params=params, headers=headers)
+            if r.json().get('status_code') == 401:
+                print(f"{r.json().get('message')}")
+            else:
+                return r
+        except RequestException as e:
+            print(e)
 
 
 def search_tv_show_by_name(term='Star Trek', display='human'):
@@ -19,25 +44,16 @@ def search_tv_show_by_name(term='Star Trek', display='human'):
     ARG 1 = search term
     ARG 2 = display view (human or json)
     """
-    headers = {
-        'X-AppKey': TOKEN,
-    }
-    params = {
-        'term': term,
-    }
-    try:
-        r = requests.get(SERVER + '/lookup', params=params, headers=headers)
-        if r.json().get('status_code') == 401:
-            print(f"{r.json().get('message')}")
-        elif display == 'json':
+    client = RestClient(SERVER, ENDPOINT, TOKEN)
+    r = client.search_term(term)
+    if r is not None:
+        if display == 'json':
             parsed = json.loads(r.text)
             print(json.dumps(parsed, indent=4, sort_keys=True))
         else:
             for title in r.json().get('results'):
                 where_to_watch = ', '.join([item.get('display_name') for item in title.get('locations')])
                 print(f"{title.get('name')}, available here: ({where_to_watch})")
-    except RequestException as e:
-        print(e)
 
 
 if __name__ == '__main__':
