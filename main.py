d07da802b8af45525f76212e4a2467ffa3741696
Denis Matakov <denis@matakov.com>
2021-05-12 20:53:35 +0300

    Added logging to see errors and warnings and get_locations method to reduce out of class logic

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 import fire
@@ -17,6 +18,12 @@ class Movie:
     def __init__(self, **kwargs):
         """Accept all dict fields as properties."""
         self.__dict__.update(kwargs)
+
+    def get_locations(self):
+        """Return comma separated string of Video Services."""
+        return ', '.join(
+            [location['display_name'] for location in self.locations]
+        )
 
 
 class RestClient:
@@ -41,26 +48,34 @@ class RestClient:
             )
             return response
         except RequestException as e:
-            print(e)
+            logging.error(e)
 
     @staticmethod
     def _response_code_is_valid(response):
-        return response.json().get('status_code') == 200
+        return response.json().get('status_code')
 
     def lookup_movies(self, term, endpoint='/lookup'):
         """Returns list of movies objects according search term."""
         response = self._get_response(endpoint, term=term)
         if response is not None:
-            if self._response_code_is_valid(response):
+            status_code = self._response_code_is_valid(response)
+            if status_code == 200:
                 return self._convert_to_movies_2(response)
+            else:
+                logging.error(f'API status code is {status_code}')
+        else:
+            logging.error(f'No response at all. Check server endpoint.')
 
     @staticmethod
     def _convert_to_movies_2(response):
         """Keep movies as object to simplify properties lookup."""
         movies = []
-        data = response.json()['results']
-        for item in data:
-            movies.append(Movie(**item))
+        data = response.json().get('results')
+        if data:
+            for item in data:
+                movies.append(Movie(**item))
+        else:
+            logging.info('Response has no "results" section.')
         return movies
 
 
@@ -72,15 +87,12 @@ def search_tv_show_by_name(term='Star Trek'):
     """
     client = RestClient(server=SERVER, token=TOKEN)
     movies = client.lookup_movies(term, endpoint='/lookup')
-    if movies is not None:
+    if movies:
         for title in movies:
-            where_to_watch = ', '.join(
-                list(map(lambda x: x['display_name'], title.locations))
-            )
-            print(f"{title.name}, available here: ({where_to_watch})")
+            print(
+                f"{title.name}, available here: {title.get_locations()}")
     else:
-        print("Something went wrong, can't get movies. "
-              "Check API token and endpoint")
+        print("No results found.")
 
 
 if __name__ == '__main__':
