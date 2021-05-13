6d6164816df28779102892424f339d452ca99628
vincent-utelly <vincent@utelly.com>
2021-05-13 14:47:31 +0300

    force

--- main.py
+++ main.py
@@ -1,31 +1,38 @@
-import json
 import logging
 import os
-from types import SimpleNamespace
 
 import fire
 import requests
 from dotenv import load_dotenv
-from requests import RequestException
 
 load_dotenv()
 
-TOKEN = os.getenv('TOKEN')
-SERVER = os.getenv('SERVER')
+TOKEN = os.getenv("TOKEN")
+SERVER = os.getenv("SERVER")
 
 
-class Movie(SimpleNamespace):
+class Location:
+    def __init__(self, *args, **kwargs):
+        self.display_name = None
+        self.__dict__.update(kwargs)
+
+
+class Movie:
     """Movie object."""
+
+    def __init__(self, *args, **kwargs):
+        self._name, self.locations = None, []
+        kwargs["_name"] = kwargs.pop("name", None)
+        self.__dict__.update(kwargs)
+        self.locations = [Location(**location) for location in self.locations]
 
     def get_locations(self):
         """Return comma separated string of Video Services."""
-        return ', '.join(
-            [location.display_name for location in self.locations]
-        )
+        return ", ".join([location.display_name for location in self.locations])
 
     @property
-    def movie_name(self):
-        return self.name
+    def name(self):
+        return self._name
 
 
 class RestClient:
@@ -38,63 +45,49 @@ class RestClient:
 
     def _get_headers(self):
         """Build headers for authentication."""
-        return {'X-AppKey': self.token}
+        return {"X-AppKey": self.token}
 
     def _get_response(self, endpoint, **params):
         """Get response object from custom endpoint."""
-        try:
-            response = requests.get(
-                self.server + endpoint,
-                params=params,
-                headers=self.headers
+        response = requests.get(
+            self.server + endpoint, params=params, headers=self.headers
+        )
+        if response.status_code in [200, 201]:
+            return response.json()
+        else:
+            logging.error(
+                f"API status code is {response.status_code}\n"
+                f"Server said: {response.reason}"
             )
-            return response
-        except RequestException as e:
-            logging.error(e)
+            raise Exception("Request Failed")
 
-    def lookup_movies(self, term, endpoint='/lookup'):
+    def lookup_movies(self, term, endpoint="/lookup"):
         """Returns list of movies objects according search term."""
         response = self._get_response(endpoint, term=term)
-        if response is not None:
-            if response.status_code == 200:
-                return self._convert_to_movies_2(response)
-            else:
-                logging.error(f'API status code is {response.status_code}\n'
-                              f'Server said: {response.reason}')
-        else:
-            logging.error(f'No response at all. Check server endpoint.')
+        return self._convert_to_movies_2(response)
 
     @staticmethod
     def _convert_to_movies_2(response):
         """Keep movies as object to simplify properties lookup."""
-        movies = []
-        data = response.json().get('results')
-        if data:
-            for item in data:
-                movies.append(
-                    json.loads(json.dumps(item),
-                               object_hook=lambda d: Movie(**d))
-                )
-        else:
-            logging.info('Response has no "results" section.')
-        return movies
+        for item in response.get("results", []):
+            yield Movie(**item)
 
 
-def search_tv_show_by_name(term='Star Trek'):
+def search_tv_show_by_name(term="Star Trek"):
     """
     Perform search in movie database.
 
     ARG 1 = search term
     """
     client = RestClient(server=SERVER, token=TOKEN)
-    movies = client.lookup_movies(term, endpoint='/lookup')
+    movies = client.lookup_movies(term)
     if movies:
         for title in movies:
-            yield f"{title.movie_name}, available here: {title.get_locations()}"
+            yield f"{title.name}, available here: {title.get_locations()}"
     else:
         yield "No results found."
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Using Google Fire to create simple CLI
     fire.Fire(search_tv_show_by_name)
